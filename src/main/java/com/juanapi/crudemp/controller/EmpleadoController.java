49d0fc353dc3e4742a7133ce019bb85731f0712d
juanc-Baez <juanbaezc8@gmail.com>
2025-05-05 03:10:53 +0000

    Configuraciones minimas

--- src/main/java/com/juanapi/crudemp/controller/EmpleadoController.java
+++ src/main/java/com/juanapi/crudemp/controller/EmpleadoController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/empleado")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class EmpleadoController {
 
 
